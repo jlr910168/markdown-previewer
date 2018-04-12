4eeb5b1de4a61fbbf1c2d4e7a96ea3209b396fb0
Pavel <jlr910168@gmail.com>
2018-04-12 21:40:27 +0300

    add marked options, change link renderer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 import marked from 'marked';
 import example from './markdown-example.txt';
+import './style.css';
 
 const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
 
+const renderer = new marked.Renderer();
+renderer.link = (href, title, text) =>
+  `<a
+    target="_blank"
+    href="${href}"
+    ${title ? `title="${title}"` : ''}
+  >${text}</a>`;
+
+const markdownOptions = {
+  headerIds: false,
+  gfm: true,
+  baseUrl: 0,
+  breaks: true,
+  renderer,
+};
+
 function previewMarkdown() {
   const text = editor.value;
-  preview.innerHTML = marked(text);
+  preview.innerHTML = marked(text, markdownOptions);
 }
 
-function onLoad() {
+function displayExample() {
   editor.value = example;
   previewMarkdown();
 }
 
 editor.focus();
 editor.addEventListener('keyup', previewMarkdown);
-window.addEventListener('load', onLoad);
+window.addEventListener('load', displayExample);
